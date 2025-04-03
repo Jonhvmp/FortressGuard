bb8c95ee5af23be9299b6b9bfef5f3774bb85142
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2025-04-03 08:54:28 -0300

    feat: add configuração inicial do servidor Express com middlewares e rotas

---
+++ src/app.js
@@ -0,0 +1,53 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import compression from "compression";
+import environment from "./config/environment";
+import logger from "./utils/logger";
+import { error } from "winston";
+
+const app = express();
+
+// midd
+app.use(helmet()); // seg c/ headers http
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// midd de logging das req
+app.use(morgan(environment.isProd ? "combined" : "dev", {
+  stream: {
+    write: (message) => logger.info(message.trim()),
+  },
+}));
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'UP',
+    timestamp: new Date().toISOString(),
+    environment: environment.nodeEnv,
+  })
+});
+
+// NotFound
+app.use((req, res) => {
+  res.status(404).json({
+    status: 'Not Found',
+    message: `Route ${req.method} ${req.url} not found`,
+  });
+});
+
+// midd err
+app.use((err, req, res, next) => {
+  logger.error(`${err.name}: ${err.message}`);
+
+  res.status(err.status || 500).json({
+    error: err.name || 'InternalServerError',
+    message: environment.isProd ? 'An unexpected error occurred' : err.message,
+    ...(environment.isProd ? {} : { stack: err.stack }),
+  });
+});
+
+export default app;
